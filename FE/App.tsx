b0eb442d8ecc196e29d964ca63cb6d94661ead3b
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2025-08-04 16:35:53 +0900

    Delete: test용 주석 삭제

--- FE/App.tsx
+++ FE/App.tsx
@@ -73,7 +73,6 @@ export default function App() {
       />
       <NavigationContainer>
         <AuthNavigator />
-        {/* <StoreScreen></StoreScreen> */}
       </NavigationContainer>
     </View>
   );
