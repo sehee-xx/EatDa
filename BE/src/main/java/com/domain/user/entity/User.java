5c8bfcb4f4b77bdeabff3448bf6de948ac29bb65
jewan100 <hawaii1468@gmail.com>
2025-08-04 17:19:11 +0900

    feat: 회원 역할과 OAuth Provider Enum 정의 및 User Entity 생성

---
+++ BE/src/main/java/com/domain/user/entity/User.java
@@ -0,0 +1,52 @@
+package com.domain.user.entity;
+
+import com.domain.user.constants.Provider;
+import com.domain.user.constants.Role;
+import com.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(length = 255, unique = true)
+    private String email;
+
+    @NotNull
+    @Column(length = 255)
+    private String password;
+
+    @NotNull
+    @Column(length = 50, unique = true)
+    private String nickName;
+
+    @NotNull
+    @Column(length = 20)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(length = 20)
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+
+    @Column(length = 100)
+    private String providerId;
+}
