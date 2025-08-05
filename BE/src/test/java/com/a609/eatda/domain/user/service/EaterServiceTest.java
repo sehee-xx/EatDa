6111a26787c74b7cc45b0375c533a2f198f7c95a
jewan100 <hawaii1468@gmail.com>
2025-08-05 23:39:07 +0900

    test: Eater 회원가입 관련 테스트 코드 작성

---
+++ BE/src/test/java/com/a609/eatda/domain/user/service/EaterServiceTest.java
@@ -0,0 +1,136 @@
+package com.a609.eatda.domain.user.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+
+import com.domain.user.constants.Role;
+import com.domain.user.dto.request.EaterSignUpRequest;
+import com.domain.user.entity.User;
+import com.domain.user.mapper.EaterMapper;
+import com.domain.user.repository.EaterRepository;
+import com.domain.user.service.impl.EaterServiceImpl;
+import com.global.constants.ErrorCode;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EaterServiceTest {
+
+    @Mock
+    private EaterRepository eaterRepository;
+
+    @Mock
+    private EaterMapper eaterMapper;
+
+    @InjectMocks
+    private EaterServiceImpl eaterService;
+
+    private EaterSignUpRequest createEaterSingUpRequest(String email, String password, String confirmPassword,
+                                                        String nickname, List<Long> foodTagIds, String customFoodTag) {
+        return new EaterSignUpRequest(email, password, confirmPassword, nickname, foodTagIds, customFoodTag);
+    }
+
+    private void assertRegisterEaterThrows(EaterSignUpRequest request, ErrorCode errorCode) {
+        assertThatThrownBy(() -> eaterService.registerEater(request))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(errorCode.getMessage());
+    }
+
+    @Test
+    void 회원가입_성공() {
+        EaterSignUpRequest request = createEaterSingUpRequest("email@email.com", "password", "password", "nickname",
+                null, null);
+        User mockUser = User.builder()
+                .email("email@email.com")
+                .password("password")
+                .nickname("nickname")
+                .role(Role.EATER)
+                .build();
+
+        given(eaterMapper.toEntity(request)).willReturn(mockUser);
+        given(eaterRepository.save(mockUser)).willReturn(mockUser);
+
+        User result = eaterService.registerEater(request);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getEmail()).isEqualTo("email@email.com");
+        assertThat(result.getPassword()).isEqualTo("password");
+        assertThat(result.getNickname()).isEqualTo("nickname");
+        assertThat(result.getRole()).isEqualTo(Role.EATER);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" "})
+    void 회원가입_실패_이메일_누락(String invalidEmail) {
+        EaterSignUpRequest request = createEaterSingUpRequest(invalidEmail, "password", "password", "nickname", null,
+                null);
+        assertRegisterEaterThrows(request, ErrorCode.EMAIL_REQUIRED);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"invalid", "no-at.com", "abc@com"})
+    void 회원가입_실패_이메일_형식(String invalidEmail) {
+        EaterSignUpRequest request = createEaterSingUpRequest(invalidEmail, "password", "password", "nickname", null,
+                null);
+        assertRegisterEaterThrows(request, ErrorCode.EMAIL_INVALID_FORMAT);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {""})
+    void 회원가입_실패_비밀번호_누락(String invalidPassword) {
+        EaterSignUpRequest request = createEaterSingUpRequest("email@email.com", invalidPassword, invalidPassword,
+                "nickname",
+                null, null);
+        assertRegisterEaterThrows(request, ErrorCode.PASSWORD_REQUIRED);
+    }
+
+    @Test
+    void 회원가입_실패_비밀번호_불일치() {
+        EaterSignUpRequest request = createEaterSingUpRequest("email@email.com", "password", "passw0rd", "nickname",
+                null, null);
+        assertRegisterEaterThrows(request, ErrorCode.PASSWORD_MISMATCH);
+    }
+
+    @Test
+    void 회원가입_실패_비밀번호_길이가_짧음() {
+        EaterSignUpRequest request = createEaterSingUpRequest("email@email.com", "pw", "pw", "nickname", null, null);
+        assertRegisterEaterThrows(request, ErrorCode.PASSWORD_TOO_SHORT);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 회원가입_실패_닉네임_누락(String invalidNickname) {
+        EaterSignUpRequest request = createEaterSingUpRequest("email@email.com", "password", "password",
+                invalidNickname, null,
+                null);
+        assertRegisterEaterThrows(request, ErrorCode.NICKNAME_REQUIRED);
+    }
+
+    @Test
+    void 회원가입_실패_이메일_중복() {
+        EaterSignUpRequest request = createEaterSingUpRequest("email@email.com", "password", "password", "nickname",
+                null,
+                null);
+        given(eaterRepository.existsByEmail("email@email.com")).willReturn(true);
+        assertRegisterEaterThrows(request, ErrorCode.EMAIL_DUPLICATED);
+    }
+
+    @Test
+    void 회원가입_실패_닉네임_중복() {
+        EaterSignUpRequest request = createEaterSingUpRequest("email@email.com", "password", "password", "nickname",
+                null,
+                null);
+        given(eaterRepository.existsByNickname("nickname")).willReturn(true);
+        assertRegisterEaterThrows(request, ErrorCode.NICKNAME_DUPLICATED);
+    }
+}
