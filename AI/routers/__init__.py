ef264a7a15a3eddce5956f956a00cd52850e483d
DESKTOP-DLOPF19\SSAFY <tkddls0127@naver.com>
2025-08-07 18:09:03 +0900

    feat: ocr_router 추가

--- AI/routers/__init__.py
+++ AI/routers/__init__.py
@@ -3,5 +3,6 @@ Routers package
 """
 
 from .generate import router as generate_router
+from .ocr import router as ocr_router
 
-__all__ = ["generate_router"]
+__all__ = ["generate_router", "ocr_router"]
