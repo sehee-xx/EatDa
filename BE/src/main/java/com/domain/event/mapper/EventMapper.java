1f579843cfbec4a11da15abf9a4be0f8a5eedeab
Ukjong <xb253q@gmail.com>
2025-08-08 22:14:55 +0900

    feat: EventMapper에 EventAsset 관련 매핑 메서드 추가
    - toPendingEventAsset: EventAsset 엔티티 생성 매핑
    - toRequestResponse: EventAsset 응답 DTO 변환
    - AssetType 필드 매핑 지원

--- BE/src/main/java/com/domain/event/mapper/EventMapper.java
+++ BE/src/main/java/com/domain/event/mapper/EventMapper.java
@@ -1,7 +1,11 @@
 package com.domain.event.mapper;
 
+import com.domain.event.dto.request.EventAssetCreateRequest;
+import com.domain.event.dto.response.EventAssetRequestResponse;
 import com.domain.event.entity.Event;
+import com.domain.event.entity.EventAsset;
 import com.domain.store.entity.Store;
+import com.global.constants.AssetType;
 import com.global.constants.Status;
 import org.mapstruct.Mapper;
 
@@ -17,5 +21,18 @@ public interface EventMapper {
                 .endDate(endDate)
                 .status(Status.PENDING)
                 .build();
+    }
+
+    default EventAsset toPendingEventAsset(final Event event, final AssetType type, final EventAssetCreateRequest request) {
+        return EventAsset.builder()
+                .event(event)
+                .type(type)
+                .prompt(request.prompt())
+                .status(Status.PENDING)
+                .build();
+    }
+
+    default EventAssetRequestResponse toRequestResponse(final EventAsset asset) {
+        return new EventAssetRequestResponse(asset.getId());
     }
 }
