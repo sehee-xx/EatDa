23c8b95f6fc305b81c7b13a408d389b91eaf74b9
jewan100 <hawaii1468@gmail.com>
2025-08-09 16:47:14 +0900

    feat: Auth관련 JWT를 Redis에 저장하기 위한 기능 추가

--- BE/src/main/java/com/domain/auth/repository/AuthRepository.java
+++ BE/src/main/java/com/domain/auth/repository/AuthRepository.java
@@ -1,4 +1,10 @@
 package com.domain.auth.repository;
 
 public interface AuthRepository {
+
+    void saveToken(String email, String refreshToken);
+
+    String getToken(String email);
+
+    void deleteToken(String email);
 }
--- BE/src/main/java/com/domain/auth/repository/Impl/AuthRepositoryImpl.java
+++ BE/src/main/java/com/domain/auth/repository/Impl/AuthRepositoryImpl.java
@@ -1,8 +1,31 @@
 package com.domain.auth.repository.Impl;
 
+import com.domain.auth.jwt.JwtProperties;
 import com.domain.auth.repository.AuthRepository;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class AuthRepositoryImpl implements AuthRepository {
+
+    private final StringRedisTemplate redisTemplate;
+    private final JwtProperties jwtProperties;
+
+    @Override
+    public void saveToken(final String email, final String refreshToken) {
+        redisTemplate.opsForValue().set(email, refreshToken, jwtProperties.getRefreshExpiration(), TimeUnit.SECONDS);
+    }
+
+    @Override
+    public String getToken(final String email) {
+        return redisTemplate.opsForValue().get(email);
+    }
+
+    @Override
+    public void deleteToken(final String email) {
+        redisTemplate.delete(email);
+    }
 }
