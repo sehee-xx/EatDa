53a58f3d323caa0f110904fa1a3c8e0cb903b498
jewan100 <hawaii1468@gmail.com>
2025-08-09 16:50:23 +0900

    feat: AuthService가 실제 JWT와 Redis 연동 가능하도록 변경

--- BE/src/main/java/com/domain/auth/service/AuthService.java
+++ BE/src/main/java/com/domain/auth/service/AuthService.java
@@ -1,11 +1,15 @@
 package com.domain.auth.service;
 
 import com.domain.auth.dto.request.SignInRequest;
+import com.domain.auth.dto.request.SignOutRequest;
+import com.domain.auth.dto.request.TokenRequest;
 import com.domain.auth.jwt.Jwt;
 
 public interface AuthService {
 
     Jwt signIn(SignInRequest request);
 
-    void signOut();
+    void signOut(SignOutRequest request);
+
+    String reissueToken(TokenRequest request);
 }
