9f9de5ab20eec3bd83249790d21265bb9df872b2
Ukj0ng <xb253q@gmail.com>
2025-08-09 16:35:03 +0900

    feat: EventService에 handleEventAssetCallback 메서드 추가

--- BE/src/main/java/com/domain/event/service/EventService.java
+++ BE/src/main/java/com/domain/event/service/EventService.java
@@ -2,8 +2,10 @@ package com.domain.event.service;
 
 import com.domain.event.dto.request.EventAssetCreateRequest;
 import com.domain.event.dto.response.EventAssetRequestResponse;
+import com.global.dto.request.AssetCallbackRequest;
 
 public interface EventService {
 
     EventAssetRequestResponse requestEventAsset(final EventAssetCreateRequest request, final Long userId);
+    void handleEventAssetCallback(final AssetCallbackRequest<?> request);
 }
