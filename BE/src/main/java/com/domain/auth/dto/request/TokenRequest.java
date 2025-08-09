77d0ac998233f561fd4ec50e2e2f0a4f53f3f103
김건학 <kkh10002@naver.com>
2025-08-09 23:34:12 +0900

    feat: 민감 정보 필드에 @Sensitive 애노테이션 추가

--- BE/src/main/java/com/domain/auth/dto/request/TokenRequest.java
+++ BE/src/main/java/com/domain/auth/dto/request/TokenRequest.java
@@ -1,5 +1,6 @@
 package com.domain.auth.dto.request;
 
+import com.global.annotation.Sensitive;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
@@ -7,6 +8,7 @@ public record TokenRequest(
 
         @NotBlank(message = "UNAUTHORIZED")
         @Schema(description = "리프레시 토큰")
+        @Sensitive
         String refreshToken
 ) {
 }
--- BE/src/main/java/com/domain/auth/dto/response/TokenResponse.java
+++ BE/src/main/java/com/domain/auth/dto/response/TokenResponse.java
@@ -1,6 +1,9 @@
 package com.domain.auth.dto.response;
 
+import com.global.annotation.Sensitive;
+
 public record TokenResponse(
+        @Sensitive
         String accessToken
 ) {
 }
