08f0a4a31a9d9aa8ff725b2c5fd3e6581bacdfb9
Ukj0ng <xb253q@gmail.com>
2025-08-09 16:33:56 +0900

    refactor: EventAsset 엔티티 BaseAssetEntity 상속 구조로 리팩토링

--- BE/src/main/java/com/domain/event/entity/EventAsset.java
+++ BE/src/main/java/com/domain/event/entity/EventAsset.java
@@ -2,9 +2,8 @@ package com.domain.event.entity;
 
 import com.global.constants.AssetType;
 import com.global.constants.Status;
-import com.global.entity.BaseEntity;
+import com.global.entity.BaseAssetEntity;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -14,27 +13,12 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class EventAsset extends BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class EventAsset extends BaseAssetEntity {
 
     @NotNull
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "event_id")
     private Event event;
-
-    @Enumerated(EnumType.STRING)
-    private AssetType type;
-
-    private String assetUrl;
-
-    @NotBlank
-    private String prompt;
-
-    @Enumerated(EnumType.STRING)
-    private Status status = Status.PENDING;
 
     @Builder
     public EventAsset(final Event event, final AssetType type, final String assetUrl, final String prompt, final Status status) {
