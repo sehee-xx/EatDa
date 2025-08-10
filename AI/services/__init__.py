97bacbdcbed72de8f1863fb753fa11f2e3958bdd
SangIn <tkddls0127@naver.com>
2025-08-10 17:58:16 +0900

    feat: services package 수정

--- AI/services/__init__.py
+++ AI/services/__init__.py
@@ -3,8 +3,15 @@ Services package
 """
 
 from .luma_service import luma_service
+from .runway_service import runway_service
 from .gpt_service import gpt_service
 from .callback_service import callback_service
 from .ocr_menuboard_service import ocr_menuboard_service
 
-__all__ = ["luma_service", "gpt_service", "callback_service", "ocr_menuboard_service"]
+__all__ = [
+    "luma_service",
+    "runway_service",
+    "gpt_service",
+    "callback_service",
+    "ocr_menuboard_service",
+]
