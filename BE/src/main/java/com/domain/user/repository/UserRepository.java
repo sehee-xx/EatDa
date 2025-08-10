764324e927b5e8dbf43536008e0e8eb562642c62
Ukj0ng <xb253q@gmail.com>
2025-08-11 01:27:37 +0900

    chore: UserRepository에 @Repository 어노테이션 추가

--- BE/src/main/java/com/domain/user/repository/UserRepository.java
+++ BE/src/main/java/com/domain/user/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.domain.user.repository;
 
 import com.domain.user.entity.User;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository {
 
     boolean existsByEmail(String email);
