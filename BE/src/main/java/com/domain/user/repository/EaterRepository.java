6d7b7f0a3cf31bb73ce01a1123b50e9f229653d9
Ukjong <xb253q@gmail.com>
2025-08-11 15:43:49 +0900

    feat: @Repository 어노테이션 추가

--- BE/src/main/java/com/domain/user/repository/EaterRepository.java
+++ BE/src/main/java/com/domain/user/repository/EaterRepository.java
@@ -2,6 +2,8 @@ package com.domain.user.repository;
 
 import com.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EaterRepository extends JpaRepository<User, Long>, UserRepository {
 }
