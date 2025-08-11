fbf19a1ff4995fe1a29a13bd8a96b049ab0f7d5f
DESKTOP-DLOPF19\SSAFY <tkddls0127@naver.com>
2025-08-11 15:16:24 +0900

    chore: 메뉴판 ocr 파일 추가

--- AI/routers/__init__.py
+++ AI/routers/__init__.py
@@ -1,7 +1,7 @@
 """
 Routers package
 """
-from .ocr import router as ocr_router
 from .stream_test import router as stream_test_router
+from .menuboard_ocr import router as menuboard_ocr_router
 
-__all__ = ["ocr_router", "stream_test_router"]
+__all__ =  ["stream_test_router", "menuboard_ocr_router"]
