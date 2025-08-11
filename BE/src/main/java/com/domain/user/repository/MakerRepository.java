79c9ef399420d4cb1ac7fab48bdfcccc7bcd49f4
Ukjong <xb253q@gmail.com>
2025-08-11 15:56:37 +0900

    refactor: 불필요한 어노테이션 제거

--- BE/src/main/java/com/domain/user/repository/MakerRepository.java
+++ BE/src/main/java/com/domain/user/repository/MakerRepository.java
@@ -4,6 +4,5 @@ import com.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MakerRepository extends JpaRepository<User, Long>, UserRepository {
 }
