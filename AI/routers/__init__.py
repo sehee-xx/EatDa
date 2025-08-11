e5248901b0e93ba69db896e964c506d1d115826a
DESKTOP-DLOPF19\SSAFY <tkddls0127@naver.com>
2025-08-11 09:37:43 +0900

    chore: import 패키지 수정

--- AI/routers/__init__.py
+++ AI/routers/__init__.py
@@ -2,7 +2,6 @@
 Routers package
 """
 from .ocr import router as ocr_router
-from .ocr_receipt import router as ocr_receipt_router
 from .stream_test import router as stream_test_router
 
-__all__ = ["ocr_router", "ocr_receipt_router", "stream_test_router"]
+__all__ = ["ocr_router", "stream_test_router"]
