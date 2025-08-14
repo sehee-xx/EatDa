a91acfed20a067bea9a9f6820c9a71a4b5f583de
Ukjong <xb253q@gmail.com>
2025-08-14 17:52:00 +0900

    refactor: 필요없는 @Repository 어노테이션 제거

--- BE/src/main/java/com/domain/common/repository/PoiRepository.java
+++ BE/src/main/java/com/domain/common/repository/PoiRepository.java
@@ -4,11 +4,9 @@ import com.domain.common.entity.Poi;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface PoiRepository extends JpaRepository<Poi, Long> {
 
     /**
