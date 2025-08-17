dcb34a2b42786435fa3d67785b4919921f30fee9
Ukj0ng <xb253q@gmail.com>
2025-08-17 15:08:30 +0900

    feat: MenuPosterRepository에 삭제되지 않은 엔터티 조회를 위한 메서드 추가

--- BE/src/main/java/com/domain/menu/repository/MenuPosterRepository.java
+++ BE/src/main/java/com/domain/menu/repository/MenuPosterRepository.java
@@ -3,6 +3,8 @@ package com.domain.menu.repository;
 import com.domain.menu.entity.MenuPoster;
 import com.global.constants.Status;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuPosterRepository extends JpaRepository<MenuPoster, Long> {
@@ -14,4 +16,6 @@ public interface MenuPosterRepository extends JpaRepository<MenuPoster, Long> {
     List<MenuPoster> findByUserIdAndStatus(Long userId, Status status);
 
     List<MenuPoster> findByStoreIdAndStatus(Long storeId, Status status);
+
+    Optional<MenuPoster> findByIdAndDeletedFalse(Long id);
 }
